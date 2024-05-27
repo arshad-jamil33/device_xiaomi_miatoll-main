d88b111944f04159ab53d6728c7c1d1336f2ebc8
Dhina17 <dhinalogu@gmail.com>
2024-05-27 19:19:11 +0530

    miatoll: Implement torch light control
    Change-Id: Icd32d1f6aedb55462c9df4d7cc63a2a4c4e4263e
    
    [RealJohnGalt:] modify for oneplus usage, add toggle switch
    [cyberknight777]: Adapt nodes and SEPolicy for sunny
    [kardebayan]: Adapt init and SEPolicy for miatoll
    
    Change-Id: I0b86fbeefbe3b951c5b9827a06c066dd71ada002
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -67,6 +67,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
 
 $(call soong_config_set_bool,camera,override_format_from_reserved,true)
+$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.miatoll)
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
