dde6f5b19318bd137901dc97841e481735e362bf
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-25 00:19:16 +0800

    miatoll: Migrate to QTI USB Gadget AIDL HAL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- device.mk
+++ device.mk
@@ -345,7 +345,8 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb.gadget-service.qti
+    android.hardware.usb.gadget-service.qti \
+    usb_compositions.conf
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
